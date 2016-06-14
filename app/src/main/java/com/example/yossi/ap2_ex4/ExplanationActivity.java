05e87405e0495a5445cabbaebdf52e2d8d6846b1
Yossi Silberhaft <yossisilb@gmail.com>
2016-06-14 09:02:28 +0300

    added comments

--- app/src/main/java/com/example/yossi/ap2_ex4/ExplanationActivity.java
+++ app/src/main/java/com/example/yossi/ap2_ex4/ExplanationActivity.java
@@ -1,28 +1,28 @@
 package com.example.yossi.ap2_ex4;
-
-import android.content.Context;
-import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import com.kassisdion.library.ViewPagerWithIndicator;
+import android.os.Bundle;
 
 
+/****************************************************************************
+ * This Class will display the page viewer of the 5 explanation fragments
+ ***************************************************************************/
 public class ExplanationActivity extends FragmentActivity {
     ViewPager viewPager;
     PagerAdapter adapter;
-    Context context = this;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.explanation);
+        //Creates a new Adapter for the pageViewer
         adapter =  new PagerAdapter(getSupportFragmentManager());
         viewPager = (ViewPager)findViewById(R.id.pager);
         viewPager.setAdapter(adapter);
-        ViewPagerWithIndicator vwith = (ViewPagerWithIndicator) findViewById(R.id.viewPagerWithIndicator);
-        vwith.setViewPager(viewPager);
+        //Creates a PageViewer with an indicator
+        ViewPagerWithIndicator wIndicator = (ViewPagerWithIndicator) findViewById(R.id.viewPagerWithIndicator);
+        wIndicator.setViewPager(viewPager);
     }
 }
-
-
