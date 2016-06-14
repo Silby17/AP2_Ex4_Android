ecc9b1af23afb24c78053589cc8cf5b84e232188
Yossi Silberhaft <yossisilb@gmail.com>
2016-06-14 08:50:19 +0300

    done

--- app/src/main/java/com/example/yossi/ap2_ex4/ExplanationActivity.java
+++ app/src/main/java/com/example/yossi/ap2_ex4/ExplanationActivity.java
@@ -1,24 +1,27 @@
 package com.example.yossi.ap2_ex4;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
+import com.kassisdion.library.ViewPagerWithIndicator;
 
-import me.relex.circleindicator.CircleIndicator;
 
 public class ExplanationActivity extends FragmentActivity {
     ViewPager viewPager;
+    PagerAdapter adapter;
+    Context context = this;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.explanation);
+        adapter =  new PagerAdapter(getSupportFragmentManager());
         viewPager = (ViewPager)findViewById(R.id.pager);
-        //CircleIndicator indicator = (CircleIndicator) viewPager.findViewById(R.id.indicator);
-        PagerAdapter pAdapter = new PagerAdapter(getSupportFragmentManager());
-        viewPager.setAdapter(pAdapter);
-        //indicator.setViewPager(viewPager);
+        viewPager.setAdapter(adapter);
+        ViewPagerWithIndicator vwith = (ViewPagerWithIndicator) findViewById(R.id.viewPagerWithIndicator);
+        vwith.setViewPager(viewPager);
     }
 }
 
