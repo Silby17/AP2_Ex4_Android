cb69cb15369fe907f21d4a7c39117b35a3b8eae7
NAVAJAVA <nshemoul@gmail.com>
2016-06-19 16:27:51 +0300

    toast when phone is shaked

--- app/src/main/java/com/example/yossi/ap2_ex4/MessageActivity.java
+++ app/src/main/java/com/example/yossi/ap2_ex4/MessageActivity.java
@@ -1,16 +1,25 @@
 package com.example.yossi.ap2_ex4;
 
-import android.content.pm.LabeledIntent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
 import android.widget.TextView;
+import android.widget.Toast;
+
 
 /**
  * Created by Nava on 15/06/2016.
  */
 public class MessageActivity extends AppCompatActivity {
+    private SensorManager sensorMgr;
+    private ShakeListener listener;
+    private Sensor accelerometer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +35,60 @@ public class MessageActivity extends AppCompatActivity {
         textview.setTextSize(20);
         ab.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         ab.setCustomView(textview);
+        //sensor
+        sensorMgr = (SensorManager) getSystemService(SENSOR_SERVICE);
+        accelerometer = sensorMgr.getDefaultSensor(SensorManager.SENSOR_ACCELEROMETER);
+        listener = new ShakeListener();
 
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorMgr.registerListener(listener, accelerometer, SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorMgr.unregisterListener(listener);
+    }
+
+    class ShakeListener implements SensorEventListener {
+        private static final int FORCE_THRESHOLD = 1500, TIME_THRESHOLD = 100, SHAKE_TIMEOUT = 500;
+        private static final int SHAKE_DURATION = 1000, SHAKE_COUNT = 3;
+        private float mLastX = -1.0f, mLastY = -1.0f, mLastZ = -1.0f;
+        private int mShakeCount = 0;
+        private long mLastShake, mLastForce, mLastTime;
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            long now = System.currentTimeMillis();
+            if ((now - mLastForce) > SHAKE_TIMEOUT) {
+                mShakeCount = 0;
+            }
+
+            float[] values = event.values;
+            if ((now - mLastTime) > TIME_THRESHOLD) {
+                long diff = now - mLastTime;
+                float speed = Math.abs(values[SensorManager.DATA_X] + values[SensorManager.DATA_Y] + values[SensorManager.DATA_Z] - mLastX - mLastY - mLastZ) / diff * 10000;
+                if (speed > FORCE_THRESHOLD) {
+                    if ((++mShakeCount >= SHAKE_COUNT) && (now - mLastShake > SHAKE_DURATION)) {
+                        mLastShake = now;
+                        mShakeCount = 0;
+                        Toast.makeText(MessageActivity.this, "Loading", Toast.LENGTH_LONG).show();
+                    }
+                    mLastForce = now;
+                }
+                mLastTime = now;
+                mLastX = values[SensorManager.DATA_X];
+                mLastY = values[SensorManager.DATA_Y];
+                mLastZ = values[SensorManager.DATA_Z];
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
     }
 }
