d762f7023e4f21297a3abf704acf6c42e776da5d
Yossi Silbrethaft <yossisilb@gmail.com>
2016-06-22 14:58:24 +0300

    added comments in interface and finished bus provider

--- app/src/main/java/com/example/yossi/ap2_ex4/BusProvider.java
+++ app/src/main/java/com/example/yossi/ap2_ex4/BusProvider.java
@@ -3,12 +3,11 @@ package com.example.yossi.ap2_ex4;
 import com.squareup.otto.Bus;
 
 public class BusProvider {
-
     private static final Bus BUS = new Bus();
+
+    public BusProvider(){}
 
     public static Bus getInstance(){
         return BUS;
     }
-
-    public BusProvider(){}
 }
--- app/src/main/java/com/example/yossi/ap2_ex4/Interface.java
+++ app/src/main/java/com/example/yossi/ap2_ex4/Interface.java
@@ -3,12 +3,25 @@ package com.example.yossi.ap2_ex4;
 import retrofit.Callback;
 import retrofit.http.Field;
 import retrofit.http.FormUrlEncoded;
-import retrofit.http.GET;
 import retrofit.http.POST;
-import retrofit.http.Query;
 
+
+/***************************************************************************
+ * This Interface will define all the Posting methods that will be used
+ * to send data to the web server using the POST method
+ ***************************************************************************/
 public interface Interface {
 
+
+    /**********************************************************************
+     * This will POST new user data to the server
+     * @param username - username
+     * @param password - password
+     * @param name - users name
+     * @param email - users email
+     * @param icon - icon of their choosing
+     * @param serverResponseCallback - The response result from server
+     **********************************************************************/
     @FormUrlEncoded
     @POST("/NewUser")
     void postNewUser(@Field("username") String username,
@@ -19,7 +32,13 @@ public interface Interface {
                      Callback<ResultResponse> serverResponseCallback);
 
 
-    //This method is used for "POST"
+    /**********************************************************************
+     * This method will POST login data to the Web Server
+     * @param method - Method to post to
+     * @param username - username
+     * @param password - password
+     * @param serverResponseCallback login result from the server
+     ***********************************************************************/
     @FormUrlEncoded
     @POST("/login")
     void postData(@Field("method") String method,
@@ -28,10 +47,29 @@ public interface Interface {
                   Callback<ResultResponse> serverResponseCallback);
 
 
-    //This method is used for "GET"
-    @GET("/")
-    void getData(@Query("method") String method,
-                 @Query("username") String username,
-                 @Query("password") String password,
-                 Callback<ServerResponse> serverResponseCallback);
+    /**********************************************************************
+     * This Method will POST the new Message and the senders username to
+     * the Web Server
+     * @param username - username
+     * @param message - message to send
+     * @param serverResponse - Response result from the server
+     ***********************************************************************/
+    @FormUrlEncoded
+    @POST("/messageServlet")
+    void postNewMessage(@Field("username") String username,
+                        @Field("message") String message,
+                        Callback<ResultResponse> serverResponse);
+
+
+    /**********************************************************************
+     * This method will POST a request to the the Server to get a specific
+     * message with a given ID
+     * @param id - ID of the message to get
+     * @param serverResponse - response from server with the msg or with
+     *                       the failed report
+     ***********************************************************************/
+    @FormUrlEncoded
+    @POST("/getMessage")
+    void postGetMessage(@Field("msgID") String id,
+                        Callback<GetMessageResponse> serverResponse);
 }
